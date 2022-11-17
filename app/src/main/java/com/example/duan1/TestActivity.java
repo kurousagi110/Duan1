4fc8c83a6b8da5119d7d834e37598d36db67fd5f
kurousagi110 <110773925+kurousagi110@users.noreply.github.com>
2022-11-17 12:30:22 +0700

    update grade chart

--- app/src/main/java/com/example/duan1/TestActivity.java
+++ app/src/main/java/com/example/duan1/TestActivity.java
@@ -11,15 +11,15 @@ import com.example.duan1.database.DataHelper;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
         DataHelper dataHelper = new DataHelper(this);
         ThuChiDAO thuChiDAO = new ThuChiDAO(this);
-        ArrayList<KhoanThuChi> list = thuChiDAO.getDSKhoanThuChi("thu",3);
+        ArrayList<KhoanThuChi> list = thuChiDAO.getDSKhoanThuChi("chi",3);
         int a = list.size();
         Toast.makeText(this, a, Toast.LENGTH_SHORT).show();
     }

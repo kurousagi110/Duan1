79858648969b5172f253415f3ade842a5810fc5a
nguyenbaloi1 <110796542+nguyenbaloi1@users.noreply.github.com>
2022-11-22 16:32:07 +0700

    test

--- app/src/main/java/com/example/duan1/Login_Activity.java
+++ app/src/main/java/com/example/duan1/Login_Activity.java
@@ -1,73 +1,47 @@
 package com.example.duan1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Login_Activity extends AppCompatActivity {
-    IntentFilter intentFilter;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class Login_Activity extends AppCompatActivity {
+    EditText edUserName, edPassword;
+    Button btnLogin;
+    CheckBox chkRememberPass;
+    String strUser, strPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dangnhap);
+        setTitle("ĐĂNG NHÂP");
+        edUserName = findViewById(R.id.edtUsername);
+        edPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        chkRememberPass = findViewById(R.id.chkRememberPass);
 
-        EditText edtUser = findViewById(R.id.edtUsername);
-        EditText edtPass = findViewById(R.id.edtPassword);
-        Button bntLogin = findViewById(R.id.btnLogin);
-
-        intentFilter = new IntentFilter();
-        intentFilter.addAction("KiemTraDangNhap");
-
-
-        bntLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String user = edtUser.getText().toString();
-                String pass = edtPass.getText().toString();
-
-                Intent intent = new Intent( Login_Activity.this, KiemTraDangNhap.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("user", user);
-                bundle.putString("pass", pass);
-                intent.putExtras(bundle);
-                startService(intent);
-            }
-        });
     }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        registerReceiver(myBroadcastReceiver, intentFilter);
-    }
-
-    public BroadcastReceiver myBroadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {// nhan data
-
-            switch (intent.getAction()){
-                case "KiemTraDangNhap":
-                    Bundle bundle = intent.getExtras();
-                    boolean check  = bundle.getBoolean("check");
-                    if(check){
-                        startActivity(new Intent(Login_Activity.this, MainActivity.class));
-                    }else {
-                        Toast.makeText(context, "Dang nhap khong thanh cong",Toast.LENGTH_LONG);
-                    }
-                    break;
-                default:
-                    break;
-            }
+    public void rememberUser (String U, String P, boolean status) {
+        SharedPreferences pref = getSharedPreferences("USER_FILE", MODE_PRIVATE);
+        SharedPreferences.Editor edit = pref.edit();
+        if (!status) {
+            //хоа tinh trang luu tru truoc do
+            edit.clear();
+        } else {
+            //luu du lieu
+            edit.putString("USERNAME", U);
+            edit.putString("PASSWORD", P);
+            edit.putBoolean("REMEMBER", status);
         }
-    };
+        //luu Lai toan bo
+        edit.commit();
+    }
+
 }

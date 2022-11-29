10afc43582d4bebc91cff09b38f0417fa690daaf
kurousagi110 <110773925+kurousagi110@users.noreply.github.com>
2022-11-29 12:33:54 +0700

    chuc nang tai khoan

--- app/src/main/java/com/example/duan1/Fragment/TaiKhoan_Fragment.java
+++ app/src/main/java/com/example/duan1/Fragment/TaiKhoan_Fragment.java
@@ -2,16 +2,19 @@ package com.example.duan1.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.duan1.R;
 
 public class TaiKhoan_Fragment extends Fragment {
-
+TextView txtSoTK,txtTenTaiKhoan,txtDoiMatKhau,txtDangXuat;
     public TaiKhoan_Fragment() {
         // Required empty public constructor
     }
@@ -19,7 +22,18 @@ public class TaiKhoan_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tai_khoan_, container, false);
+        View view = inflater.inflate(R.layout.fragment_tai_khoan_, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        txtSoTK = view.findViewById(R.id.txtSoTK);
+        txtTenTaiKhoan = view.findViewById(R.id.txtTenTaiKhoan);
+        txtDoiMatKhau = view.findViewById(R.id.txtDoiMatKhau);
+        txtDangXuat = view.findViewById(R.id.txtDangXuat);
+
+
     }
 }
